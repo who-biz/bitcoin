e3379e84e5488c75b4c45984138b615e75530838
pericanikolic <perica.biz@gmail.com>
2017-09-12 23:03:41 +0200

    Test: Fork from Wormhole block 1555229 (epoch 44).

--- src/main.cpp
+++ src/main.cpp
@@ -1760,7 +1760,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
         StartOffset = generateMTRandom(wseed, 35820);
         WormholeStartBlock = StartOffset + ((epoch - 1)  * 36000); // Wormholes start from Epoch 2
 
-        if(epoch > 1 && epoch < 148 && nHeight >= WormholeStartBlock && nHeight < WormholeStartBlock + 180)
+        if(epoch > 1 && epoch < 44 && nHeight >= WormholeStartBlock && nHeight < WormholeStartBlock + 180)
         {
             nSubsidy = 2973 * COIN;
         }
